69f5514d32cb7b0b34e18d751be4359dc84a2c18
shefket-mustafa <miracle_1@abv.bg>
2025-12-16 21:49:53 +0200

    Added refetching in inventory context so that we update the dashboard with a refetch after adding an item to it

--- client/src/context/inventory/InventoryContext.tsx
+++ client/src/context/inventory/InventoryContext.tsx
@@ -2,44 +2,39 @@ import { useEffect, useState, type ReactNode } from "react";
 import { InventoryContext } from "./inventory-context-hook";
 import type { InventoryItem } from "../data/DataContext";
 
-export const InventoryProvider = ({children}: {children: ReactNode}) => {
-    const BASE_URL = import.meta.env.VITE_BASE_URL;
-    const [allItems, setAllItems] = useState<InventoryItem[]>([]);
+export const InventoryProvider = ({ children }: { children: ReactNode }) => {
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const [allItems, setAllItems] = useState<InventoryItem[]>([]);
 
-        const addItem = async (data: InventoryItem) => {
+  const addItem = async (data: InventoryItem) => {
+    console.log(BASE_URL);
+    const res = await fetch(`${BASE_URL}/inventory/addItems`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    
+    await fetchItems();
+    return res;
+  };
 
-            console.log(BASE_URL);
-            const res = await fetch(`${BASE_URL}/inventory/addItems`, {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify(data)
-            });
-            console.log(res);
-            return res
-        }
+  const fetchItems = async () => {
+    const res = await fetch(`${BASE_URL}/inventory/getItems`);
+    if (!res.ok) {
+      console.log(res);
+    }
 
-        useEffect(() => {
+    const data = await res.json();
+    setAllItems(data);
+  };
 
-           fetch(`${BASE_URL}/inventory/getItems`, {
-            method: "GET",
-            headers: {"Content-Type":"application/json"}
-           }).then((data) => data.json())
-           .then((data) => {
-            console.log(data);
-            setAllItems(data)
-            
-           })
-           
-                
-                
-            
-        },[])
-      
+  useEffect(() => {
+    fetchItems()
+  },[])
 
-    return(
-        <InventoryContext.Provider value={{addItem, allItems}}>
-            {children}
-
-        </InventoryContext.Provider>
-    )
-}  
+  return (
+    <InventoryContext.Provider value={{ addItem, allItems }}>
+      {children}
+    </InventoryContext.Provider>
+  );
+};
